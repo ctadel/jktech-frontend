5ef47786e90b9f4269c5fbed3e601be3a545d5a9
ctadel <nep.prajwal@gmail.com>
2025-05-30 21:20:25 +0530

    Updated the homepage with tailwind css and the navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { StorageService } from './_services/storage.service';
 import { AuthService } from './_services/auth.service';
@@ -12,7 +12,23 @@ import { EventBusService } from './_shared/event-bus.service';
 export class AppComponent {
   isLoggedIn = false;
   showAdminBoard = false;
-  showModeratorBoard = false;
+
+  // UI Interaction
+  isDropdownOpen = false;
+
+  @ViewChild('dropdownRef', { static: false }) dropdownRef!: ElementRef;
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    if (this.isDropdownOpen && this.dropdownRef && !this.dropdownRef.nativeElement.contains(event.target)) {
+      this.isDropdownOpen = false;
+    }
+  }
+
   user: any;
 
   eventBusSub?: Subscription;
