88b81849df3999bb33a4008095402dc5f490eea0
ctadel <nep.prajwal@gmail.com>
2025-06-02 04:17:27 +0530

    bug fix: tailwind config included

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 0.2s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        }
+      }
+    }
+  },
+  plugins: [],
+}
